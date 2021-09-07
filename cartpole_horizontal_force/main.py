7e8788e82470934c7c30402d6388e84ddd06de21
Devdhar Patel <devdharpatel@gmail.com>
2021-09-07 18:24:03 -0400

    Changes to accomodate intra-frame jitters

--- cartpole_horizontal_force/main.py
+++ cartpole_horizontal_force/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import gym
+from gym.envs.mujoco import inverted_pendulum
 import argparse
 import os
 import random
@@ -13,38 +14,67 @@ import TD3
 import OurDDPG
 import DDPG
 
+default_timestep = 0.02
+default_frame_skip = 2
+
+def jitter_step(self, a, force, frames1, frames2):
+    self.model.opt.gravity[0] = force
+    reward = 1.0
+    self.do_simulation(a, frames1)
+    self.model.opt.gravity[0] = force
+    self.do_simulation(a, frames2)
+    ob = self._get_obs()
+    notdone = np.isfinite(ob).all() and (np.abs(ob[1]) <= 0.2)
+    done = not notdone
+    return ob, reward, done, {}
+
+
+inverted_pendulum.InvertedPendulumEnv.jitter_step = jitter_step
+
 
 # Runs policy for X episodes and returns average reward
 # A fixed seed is used for the eval environment
-def eval_policy(policy, env_name, eval_episodes=10, time_change_factor=1, jit=False, env_timestep=0.02, g_ratio=1):
-    eval_env = gym.make(env_name)
-    eval_env.seed(100)
-    eval_env._max_episode_steps = 1000 * time_change_factor
-    eval_env.model.opt.timestep = env_timestep
-    lasttime = 2 * time_change_factor
+def eval_policy(policy, env_name, eval_episodes=10, time_change_factor=1, jit_duration=0, env_timestep=0.02, force=1,
+                frame_skip=1, jit_frames=0):
+    eval_env = make_env(env_name, 100, time_change_factor, env_timestep, frame_skip)
 
     avg_reward = 0.
-    if jit:
-        counter = 0
-        disturb = random.randint(50, 100)
+    if jit_duration:
+        counter = 1
+        disturb = random.randint(50, 100) * time_change_factor
         print("==> Using Horizontal Jitter!")
+        jittering = False
 
     t = 0
     for _ in range(eval_episodes):
         state, done = eval_env.reset(), False
         while not done:
             action = policy.select_action(np.array(state))
-            state, reward, done, _ = eval_env.step(action)
+            # Perform action
+            if not jittering:
+                next_state, reward, done, _ = eval_env.step(action)
+            elif jit_frames - jittered_frames < frame_skip:
+                next_state, reward, done, _ = eval_env.jitter_step(action, force, jit_frames - jittered_frames,
+                                                              frame_skip - (jit_frames - jittered_frames))
+                jittering = False
+                disturb = random.randint(50, 100) * time_change_factor
+                env.model.opt.gravity[0] = 0
+                counter = 1
+            else:
+                next_state, reward, done, _ = eval_env.step(action)
+                jittered_frames += frame_skip
+                if jittered_frames == jit_frames:
+                    jittering = False
+                    disturb = random.randint(50, 100) * time_change_factor
+                    eval_env.model.opt.gravity[0] = 0
+                    counter = 1
             avg_reward += reward
 
-            if jit:
-                if counter > 0 and counter % disturb == 0:
-                    hori_force = (g_ratio * random.random() * 9.81) * pow(-1, (random.random() > 0.5))
+            if jit_duration:
+                if counter % disturb == 0:
                     eval_env.model.opt.gravity[0] = hori_force
-                if counter > lasttime and counter % disturb == lasttime:
-                    eval_env.model.opt.gravity[0] = 0
-                    disturb = random.randint(50, 100)
-                    counter = 0
+                    jittering = True
+                    jittered_frames = 0
                 counter += 1
 
             t += 1
@@ -57,10 +87,23 @@ def eval_policy(policy, env_name, eval_episodes=10, time_change_factor=1, jit=Fa
     return avg_reward
 
 
-def train(policy='TD3', env_name='InvertedPendulum-v2', seed=0, start_timesteps=25e3, eval_freq=5e3, max_timesteps=1e5,
-          expl_noise=0.1, batch_size=256, discount=0.99, tau=0.005, policy_freq=2, policy_noise=2,noise_clip=0.5,
-          save_model=False, load_model="", jit=False, g_ratio=1, env_timestep=0.02):
-    arguments = [policy, env_name, seed, jit, g_ratio, env_timestep]
+def make_env(env_name, seed, time_change_factor, env_timestep, frameskip):
+    env = gym.make(env_name)
+    env.seed(seed)
+    env.action_space.seed(seed)
+    env._max_episode_steps = 1000 * time_change_factor
+    env.model.opt.timestep = env_timestep
+    env.frameskip = frameskip
+
+    return env
+
+
+def train(policy='TD3', seed=0, start_timesteps=25e3, eval_freq=5e3, max_timesteps=1e5,
+          expl_noise=0.1, batch_size=256, discount=0.99, tau=0.005, policy_freq=2, policy_noise=2, noise_clip=0.5,
+          save_model=False, load_model="", jit_duration=0.02, g_ratio=1, response_rate=0.04):
+    hori_force = g_ratio * 9.81
+    env_name = 'InvertedPendulum-v2'
+    arguments = [policy, env_name, seed, jit_duration, g_ratio]
     file_name = '_'.join([str(x) for x in arguments])
     print("---------------------------------------")
     print(f"Policy: {policy}, Env: {env_name}, Seed: {seed}")
@@ -72,22 +115,36 @@ def train(policy='TD3', env_name='InvertedPendulum-v2', seed=0, start_timesteps=
     if save_model and not os.path.exists("./models"):
         os.makedirs("./models")
 
-    env = gym.make(env_name)
-    time_change_factor = 0.02 / env_timestep
+    if response_rate % default_timestep == 0:
+        frame_skip = jit_duration % default_timestep
+        timestep = default_timestep
+    elif jit_duration:
+        timestep = jit_duration
+        frame_skip = response_rate / timestep
+    else:
+        timestep = response_rate
+        frame_skip = 1
+
+    if jit_duration:
+        if jit_duration % timestep == 0:
+            jit_frames = jit_duration / timestep
+        else:
+            raise ValueError("jit_duration should be a multiple of the timestep: " + str(timestep))
+
+    print('timestep:', timestep)
+    print('frameskip:', frame_skip)
+
+    time_change_factor = (default_timestep * default_frame_skip) / (timestep * frame_skip)
+    env = make_env(env_name, seed, time_change_factor, timestep, frame_skip)
 
     print('time change factor', time_change_factor)
     # Set seeds
-    env.seed(seed)
-    env.action_space.seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
 
-    env._max_episode_steps = 1000 * time_change_factor
     max_timesteps = max_timesteps * time_change_factor
     eval_freq = eval_freq * time_change_factor
     start_timesteps = start_timesteps * time_change_factor
-    env.model.opt.timestep = env_timestep
-    lasttime = time_change_factor * 2
 
 
     state_dim = env.observation_space.shape[0]
@@ -121,18 +178,20 @@ def train(policy='TD3', env_name='InvertedPendulum-v2', seed=0, start_timesteps=
     replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
 
     # Evaluate untrained policy
-    evaluations = [eval_policy(policy, env_name, eval_episodes=10, time_change_factor=time_change_factor, jit=jit,
-                               env_timestep=env_timestep, g_ratio=g_ratio)]
+    evaluations = [eval_policy(policy, env_name, eval_episodes=10, time_change_factor=time_change_factor,
+                               jit_duration=jit_duration, env_timestep=timestep, force=hori_force, frame_skip=frame_skip,
+                               jit_frames=jit_frames)]
 
     state, done = env.reset(), False
     episode_reward = 0
     episode_timesteps = 0
     episode_num = 0
 
-    if jit:
-        counter = 0
-        disturb = random.randint(50, 100)
+    if jit_duration:
+        counter = 1
+        disturb = random.randint(50, 100) * time_change_factor
         print("==> Using Horizontal Jitter!")
+        jittering = False
 
     for t in range(int(max_timesteps)):
         episode_timesteps += 1
@@ -147,7 +206,23 @@ def train(policy='TD3', env_name='InvertedPendulum-v2', seed=0, start_timesteps=
             ).clip(-max_action, max_action)
 
         # Perform action
-        next_state, reward, done, _ = env.step(action)
+        if not jittering:
+            next_state, reward, done, _ = env.step(action)
+        elif jit_frames - jittered_frames < frame_skip:
+            next_state, reward, done, _ = env.jitter_step(action, hori_force, jit_frames - jittered_frames, frame_skip - (jit_frames - jittered_frames))
+            jittering = False
+            disturb = random.randint(50, 100) * time_change_factor
+            env.model.opt.gravity[0] = 0
+            counter = 1
+        else:
+            next_state, reward, done, _ = env.step(action)
+            jittered_frames += frame_skip
+            if jittered_frames == jit_frames:
+                jittering = False
+                disturb = random.randint(50, 100) * time_change_factor
+                env.model.opt.gravity[0] = 0
+                counter = 1
+
         done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
 
         # Store data in replay buffer
@@ -173,19 +248,17 @@ def train(policy='TD3', env_name='InvertedPendulum-v2', seed=0, start_timesteps=
         # Evaluate episode
         if (t + 1) % eval_freq == 0:
             evaluations.append(
-                eval_policy(policy, env_name, eval_episodes=10, time_change_factor=time_change_factor, jit=jit,
-                            env_timestep=env_timestep, g_ratio=g_ratio))
+                eval_policy(policy, env_name, eval_episodes=10, time_change_factor=time_change_factor,
+                            jit_duration=jit_duration, env_timestep=timestep, force=hori_force, frame_skip=frame_skip,
+                            jit_frames=jit_frames))
             np.save(f"./results/{file_name}", evaluations)
             if save_model: policy.save(f"./models/{file_name}")
 
-        if jit:
-            if counter > 0 and counter % disturb == 0:
-                hori_force = (g_ratio * random.random() * 9.81) * pow(-1, (random.random() > 0.5))
+        if jit_duration:
+            if counter % disturb == 0:
                 env.model.opt.gravity[0] = hori_force
-            if counter > lasttime and counter % disturb == lasttime:
-                env.model.opt.gravity[0] = 0
-                disturb = random.randint(50, 100)
-                counter = 0
+                jittering = True
+                jittered_frames = 0
             counter += 1
 
 
@@ -197,7 +270,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--policy", default="TD3")  # Policy name (TD3, DDPG or OurDDPG)
-    parser.add_argument("--env_name", default="InvertedPendulum-v2")  # OpenAI gym environment name
     parser.add_argument("--seed", default=0, type=int)  # Sets Gym, PyTorch and Numpy seeds
     parser.add_argument("--start_timesteps", default=25e3, type=int)  # Time steps initial random policy is used
     parser.add_argument("--eval_freq", default=5e3, type=int)  # How often (time steps) we evaluate
@@ -211,9 +283,9 @@ if __name__ == "__main__":
     parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
     parser.add_argument("--save_model", action="store_true")  # Save model and optimizer parameters
     parser.add_argument("--load_model", default="")  # Model load file name, "" doesn't load, "default" uses file_name
-    parser.add_argument("--jit", action="store_true")  # Whether use jetter or not
+    parser.add_argument("--jit_duration", default=0.0, type=float, help="Duration in seconds for the horizontal force")
     parser.add_argument("--g_ratio", default=0, type=float, help='Maximum horizontal force g ratio')
-    parser.add_argument("--env_timestep", default=0.02, type=float, help="environment time between each frame")
+    parser.add_argument("--response_rate", default=0.04, type=float, help="Response time of the agent in seconds")
 
     args = parser.parse_args()
     args = vars(args)
