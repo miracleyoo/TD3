bca9862d8a77f2ae0c0de43cde65d701fc0b7e42
Devdhar Patel <devdharpatel@gmail.com>
2022-01-06 02:30:40 +0530

    bug fix

--- cartpole_horizontal_force/reflex/gpu.sh
+++ cartpole_horizontal_force/reflex/gpu.sh
@@ -14,5 +14,5 @@ response_rate=${3:-0.02}
 reflex_threshold=${4:-0.15}
 echo $seed $g_ratio $response_rate $reflex_threshold
 
-python train_reflex.py --seed $seed --g_ratio $g_ratio --jit_duration 0.02 --response_rate $response_rate --reflex_response_rate 0.02 --max_timesteps 400000 --save_model --eval_freq 10000 --reflex_threshod $reflex_threshold
+python train_reflex.py --seed $seed --g_ratio $g_ratio --jit_duration 0.02 --response_rate $response_rate --reflex_response_rate 0.02 --max_timesteps 400000 --save_model --eval_freq 10000 --reflex_threshold $reflex_threshold
 exit
