c56e23e21167fdfdc573a6dd770fb5ed30956c64
devdharpatel@gmail.com <minecraft!337>
2022-01-12 12:32:22 -0500

    bug fix

--- cartpole_horizontal_force/bulk_gpu.sh
+++ cartpole_horizontal_force/bulk_gpu.sh
@@ -9,7 +9,7 @@ do
         do
           for response_rate in 0.01 0.02 0.04 0.08 0.16 0.32 0.64
           do
-              if ! [ -f "models/TD3_InvertedPendulum-v2_${seed}_0.02_${g_force}.0_${response_rate}_1.0_False_256_final_actor"];
+              if ! [ -f "models/TD3_InvertedPendulum-v2_${seed}_0.02_${g_force}.0_${response_rate}_1.0_False_256_final_actor" ];
               then
                 sbatch gpu.sh $seed $g_force $response_rate $neurons
                 count=$((count + 1))
