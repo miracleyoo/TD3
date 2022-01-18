f0a5e6e6fa7280b578bd1bc9668638bfe534106e
devdharpatel@gmail.com <minecraft!337>
2022-01-17 19:23:14 -0500

    update scripts

--- temporal/reflex/bulk_gpu.sh
+++ temporal/reflex/bulk_gpu.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+count=0
 for seed in {0..4}
 do
     for g_force in {0..10}
@@ -12,10 +13,16 @@ do
                 do
                   for reflex_force_scale in 1:
                   do
-                    sbatch gpu.sh $seed $g_force $response_rate $reflex_threhold $reflex_response_rate $reflex_force_scale
+                    if ! [ -f "models/TD3_reflex_InvertedPendulum-v2_${seed}_0.02_${g_force}.0_${response_rate}_1.0_${reflex_response_rate}_${reflex_threhold}_${reflex_force_scale}_final_actor" ];
+                    then
+                      sbatch gpu.sh $seed $g_force $response_rate $reflex_threhold $reflex_response_rate $reflex_force_scale
+                      count=$((count + 1))
+                    fi
                   done
                 done
             done
         done
     done
 done
+
+echo Launched $count jobs
