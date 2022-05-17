d82e21ae898dc65a953424e3bc4984754abcb10a
devdharpatel@gmail.com <minecraft!337>
2022-05-17 02:32:39 -0400

    bug fix

--- temporal/hybrid/bulk_gpu.sh
+++ temporal/hybrid/bulk_gpu.sh
@@ -11,7 +11,7 @@ do
           do
             for response_rate in 0.02
             do
-                if ! [ -f "train_all/fast_TD3_InvertedPendulum-v2_${seed}_0.02_${g_force}.0_${response_rate}_1.0_True_${parent_response_rate}_final_actor" ];
+                if ! [ -f "train_all_TD3_InvertedPendulum-v2_${seed}_0.02_${g_force}.0_${response_rate}_1.0_True_${parent_response_rate}_final_actor" ];
                 then
                   sbatch gpu.sh $seed $g_force $response_rate $reward_factor $parent_response_rate
                   count=$((count + 1))
