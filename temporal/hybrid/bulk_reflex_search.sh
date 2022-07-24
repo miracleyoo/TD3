aa8998a289a5556222b63d3cd04b6427993ec709
dee0512 <devdharpatel@gmail.com>
2022-07-23 21:03:59 -0400

    update scripts

--- temporal/hybrid/bulk_reflex_search.sh
+++ temporal/hybrid/bulk_reflex_search.sh
@@ -4,12 +4,21 @@ for seed in 0
 do
   for g_force in 0
   do
-      for response_rate in 0.02
+    for response_rate in 0.02
+    do
+      for parent_response_rate in 0.04
       do
         for population in 20
         do
-            sbatch reflex_search.sh $seed $g_force $response_rate $population
+          for jit_duration in 0.02
+          do
+            for env_name in InvertedPendulum-v2
+            do
+              sbatch reflex_search.sh $seed $g_force $response_rate $population $parent_response_rate $jit_duration $env_name
+            done
+          done
         done
       done
+    done
   done
 done
--- temporal/hybrid/reflex_search.sh
+++ temporal/hybrid/reflex_search.sh
@@ -2,17 +2,21 @@
 #
 #SBATCH --partition=gypsum-titanx-phd
 #SBATCH --gres=gpu:1
-#SBATCH --time=00-10:00:00
-#SBATCH --mem=16000
+#SBATCH --time=07-00:00:00
+#SBATCH --mem=1600000
 #SBATCH --output=outputs/output_%j.out
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=24
 
 seed=${1:-0}
 g_ratio=${2:-4}
 response_rate=${3:-0.02}
 population=${4:-20}
-echo $seed $g_ratio $response_rate $population
+parent_response_rate=${5:-0.04}
+jit_duration=${6:-0.02}
+env_name=${6:-InvertedPendulum-v2}
 
-python reflex_search.py --seed $seed --g_ratio $g_ratio --response_rate $response_rate --population $population
+echo $seed $g_ratio $response_rate $population $parent_response_rate $jit_duration $env_name
+
+python reflex_search.py --seed $seed --g_ratio $g_ratio --response_rate $response_rate --population $population --parent_response_rate $parent_response_rate --jit_duration $jit_duration --env_name $env_name
 exit
 
