5b2e32a633260cc0866ce54783819067f8d1e48f
dee0512 <devdharpatel@gmail.com>
2022-07-25 14:32:41 -0400

    bug fix

--- temporal/hybrid/reflex.py
+++ temporal/hybrid/reflex.py
@@ -105,9 +105,9 @@ def train(policy='TD3', seed=0, start_timesteps=25e3, eval_freq=5e3, max_timeste
         policy = DDPG.DDPG(**kwargs)
 
     arguments = ['reflex_search', env_name, seed, float(g_ratio), 20]
-    file_name = '_'.join([str(x) for x in arguments])
-    threshold = np.load(f"./models_paper/{file_name}_thresholds.npy")
-    scale = np.load(f"./models_paper/{file_name}_scales.npy")
+    reflex_file_name = '_'.join([str(x) for x in arguments])
+    threshold = np.load(f"./models_paper/{reflex_file_name}_thresholds.npy")
+    scale = np.load(f"./models_paper/{reflex_file_name}_scales.npy")
     policy = utils.CEMReflex(env.observation_space, threshold, scale).to('cuda')
     # for child network: add parent state value as the input
     if delayed_env:
