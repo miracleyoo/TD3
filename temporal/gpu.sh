3e2176ca8c072d2d3b7aa538fde204fd690ea06e
dee0512 <devdharpatel@gmail.com>
2022-07-30 14:06:31 -0400

    bug fix

--- temporal/gpu.sh
+++ temporal/gpu.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 #
-#SBATCH --partition=1080ti-short
+#SBATCH --partition=gypsum-1080ti-phd
 #SBATCH --gres=gpu:1
-#SBATCH --time=00-04:00:00
+#SBATCH --time=00-07:00:00
 #SBATCH --mem=16000
-#SBATCH --account=rkozma
 #SBATCH --output=outputs/output_%j.out
 #SBATCH --cpus-per-task=8
 
@@ -13,5 +12,5 @@ response_rate=${2-0.02}
 env_name=${3-InvertedPendulum-v2}
 echo $seed $g_ratio $response_rate $env_name
 
-python normal.py --seed $seed --response_rate $response_rate --delayed_env --save_model -env_name $env_name
+python normal.py --seed $seed --response_rate $response_rate --delayed_env --save_model --env_name $env_name
 exit
